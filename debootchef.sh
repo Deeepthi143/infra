bfc0872c8bdd6f6e06a78abdd41d8a0bd49576e3
Deeepthi143 <p.deepthi1988@gmail.com>
2017-08-23 22:29:47 -0400

    changes in EC2 address

--- debootchef.sh
+++ debootchef.sh
@@ -3,7 +3,7 @@
 sed -i s#Defaults.*requiretty#Defaults\ \!requiretty#g /etc/sudoers
 cd /tmp/
 echo "Downloading the rpm"
-wget  http://34.227.217.144/chef-12.19.36-1.el7.x86_64.rpm
+wget  http://34.233.28.25/chef-12.19.36-1.el7.x86_64.rpm
 
 echo "Installating the rpm"
 
@@ -13,8 +13,8 @@ sudo mkdir -p /etc/chef
 
 cd /etc/chef
 
-wget http://34.227.217.144/client.rb
-wget http://34.227.217.144/knife.rb
-wget http://34.227.217.144/deepthi143.pem
+wget http://34.233.28.25/client.rb
+wget http://34.233.28.25/knife.rb
+wget http://34.233.28.25/deepthi143.pem
 chmod 400 deepthi143.pem
 sudo chef-client
